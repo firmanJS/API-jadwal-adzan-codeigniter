e3f0100e6b44de527415db18eed98522dbc81f58
Firman Abdul Hakim <fimzhakim@gmail.com>
2019-07-08 13:15:53 +0700

    tambah versi angularjs

--- application/controllers/Adzan.php
+++ application/controllers/Adzan.php
@@ -5,12 +5,14 @@ class Adzan extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('adzan');
+		$bulan = array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
+		$data['namaBulan'] = $bulan;
+		$this->load->view('adzan',$data);
 	}
 
 	public function getjadwal()
 	{
-		if($this->input->post()){
+		if($this->input->is_ajax_request()){
 					$param  = array('city' => $_POST['kota'],'country' => 'ID',
 			'method' => '11','month' => $_POST['bulan'],'year' => $_POST['tahun']);
 
@@ -54,5 +56,54 @@ class Adzan extends CI_Controller {
 		}else{
 			redirect('');
 		}
+	}
+
+	public function getjadwalAngular()
+	{
+		//if($this->input->is_ajax_request()){
+		$dt = json_decode(file_get_contents("php://input"));
+		$param  = array('city' => $dt->kota,'country' => 'ID',
+			'method' => '11','month' => $dt->bulan,'year' => $dt->tahun);
+
+		$urlApi = 'http://api.aladhan.com/v1/calendarByCity'; //Url API
+		$urlApi .='?city='.$dt->kota; // Ambil kota dari form submit
+		$urlApi .='&country=ID'; // Negara set default Indonesia
+		$urlApi .='&method=11'; // method ke aplikasi
+		/* 
+		method method untuk aplikasi
+		dokumentasi nya kunjungi https://aladhan.com/prayer-times-api#GetCalendarByCitys
+		*/
+		$urlApi .='&month='.$dt->bulan; // Ambil bulan dari form submit
+		$urlApi .='&year='.$dt->tahun; // Ambil tahun dari form submit
+
+		$ch = curl_init(); //set curl
+		curl_setopt($ch, CURLOPT_URL, $urlApi);  // Ambil Data dari API Url
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		$output = curl_exec($ch);   
+
+		$decodeData = json_decode($output); // decode data json dari api
+
+		curl_close($ch);
+
+		/* 
+		output dari $decodeData berupa array hari per bulan sekarang kita ganti ambil ke per hari
+		disini kita bisa sesuaikan sesuia dengan kebutuhan kita
+		*/
+
+		$dayNow = ltrim(date('d'),"0") - 1; //hapus 0 di depan untuk mencocokan array hari dari $decodeData dan mengurangi hari karena di array di mulai dari 0
+		$data = array();
+		$data['subuh'] = ($decodeData->code == 200 ? $decodeData->data[$dayNow]->timings->Fajr : "Tidak Tersedia");
+		$data['dzuhur'] = ($decodeData->code == 200 ? $decodeData->data[$dayNow]->timings->Dhuhr : "Tidak Tersedia");
+		$data['ashar'] = ($decodeData->code == 200 ? $decodeData->data[$dayNow]->timings->Asr : "Tidak Tersedia");
+		$data['magrib'] = ($decodeData->code == 200 ? $decodeData->data[$dayNow]->timings->Maghrib : "Tidak Tersedia");
+		$data['isya'] = ($decodeData->code == 200 ? $decodeData->data[$dayNow]->timings->Isha : "Tidak Tersedia");
+		$data['lokasi'] = $dt->kota;
+
+		$html  = $this->load->view('V_adzan',$data,true);
+		header('Content-Type: application/json');
+		echo json_encode(array('html' => $html));
+		// }else{
+		// 	redirect('');
+		// }
 	}
 }
--- application/views/adzan.php
+++ application/views/adzan.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="AdzanApp">
 <base href="<?php echo site_url();?>">
 <head>
 	<title>API Jadwal Adzan</title>
@@ -8,16 +8,17 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.7.8/angular.min.js"></script>
 </head>
 <body>
 
-	<div class="container">
+	<div class="container" ng-controller="adzanctrl">
 		<h3>Jadwal Shalat</h3>
 		<ul class="nav nav-tabs">
 			<li class="active" ><a href="#ajax" data-toggle="tab">Example Ajax & PHP</a></li>
-			<li><a href="#angular" data-toggle="tab">Example Angularjs</a></li>
+			<li><a href="#angular" ng-click="angularTab()" data-toggle="tab">Example Angularjs</a></li>
 		</ul>
-		<div class="tab-content">
+		<div class="tab-content" >
 			<div class="tab-pane active" id="ajax">
 				<div class="container">
 					<form id="submitForm" method="post">
@@ -27,7 +28,11 @@
 						</div>
 						<div class="form-group">
 							<label for="pwd">Bulan</label>
-							<input type="number" class="form-control" name="bulan">
+							<select class="form-control" name="bulan">
+							<?php $noBulan = 1; for($index=0; $index<12; $index++){ ?>
+								<option value="<?php echo $noBulan;?>"><?php echo $namaBulan[$index];?></option>
+								<?php $noBulan++; } ?>		
+							</select>
 						</div>
 						<div class="form-group">
 							<label for="pwd">Tahun</label>
@@ -43,32 +48,91 @@
 					</div>
 				</div>
 			</div>
-			<div class="tab-pane" id="angular">
-				<h3>Coming Soon !</h3>
+			<div class="tab-pane active" id="angular" >
+				<div class="container">
+					<form ng-show="FormAngular" method="post">
+						<div class="form-group">
+							<label for="email">Kota</label>
+							<input type="text" class="form-control" ng-model="kota">
+						</div>
+						<div class="form-group">
+							<label for="pwd">Bulan</label>
+							<select class="form-control" ng-model="bulan">
+							<?php $noBulan = 1; for($index=0; $index<12; $index++){ ?>
+								<option value="<?php echo $noBulan;?>"><?php echo $namaBulan[$index];?></option>
+								<?php $noBulan++; } ?>		
+							</select>
+						</div>
+						<div class="form-group">
+							<label for="pwd">Tahun</label>
+							<input type="number" class="form-control" ng-model="tahun">
+						</div>
+						<button type="button" ng-click="cekAngular()" class="btn btn-default">Submit</button>
+					</form>
+					<center id="loader" ng-show="angularLoader">
+						<img src="public/loader.gif" alt="" style="max-width: 100px;">
+					</center>
+					<div id="showData">
+						
+					</div>
+					<div ng-show="showDataAngular" ng-bind-html="GetData | safeHtml">
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 	<script type="text/javascript">
-		"use strict";
+	"use strict";
+	$("#showData").html("");
+	$("#loader").hide();
+	$("#cek").click(function(event) {
 		$("#showData").html("");
-		$("#loader").hide();
-		$("#cek").click(function(event) {
-			$("#showData").html("");
-			$("#loader").show();
-			$.ajax({
-				type: 'POST',
-				url: 'adzan/getjadwal',
-				data: $("#submitForm").serializeArray(),
-				success: function (res) {
-					$("#loader").hide();
-					$("#showData").html(res.html);
-				},
-				error: function (res) {
-					$("#showData").html("");
-					$("#loader").hide();
-				}
-			});
+		$("#loader").show();
+		$.ajax({
+			type: 'POST',
+			url: 'adzan/getjadwal',
+			data: $("#submitForm").serializeArray(),
+			success: function (res) {
+				$("#loader").hide();
+				$("#showData").html(res.html);
+			},
+			error: function (res) {
+				$("#showData").html("");
+				$("#loader").hide();
+			}
 		});
-	</script>
+	});
+	const app = angular.module('AdzanApp',[]);
+	app.controller('adzanctrl', function($http,$scope){
+		$scope.FormAngular = false;
+		$scope.showDataAngular = false;
+		$scope.angularLoader = false;
+		$scope.angularTab = function(){
+			$scope.FormAngular = true;
+		}
+		$scope.cekAngular = function(){
+			var formData = { 'kota': $scope.kota, 'bulan' : $scope.bulan ,'tahun' : $scope.tahun };
+			var postData = 'myData='+JSON.stringify(formData);
+			$scope.GetData = "";
+			$http(
+				{
+					method : 'POST',
+					url : 'adzan/getjadwalAngular',
+					data: formData,
+					headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+					
+				}).then(function successCallback(response) {
+					$scope.showDataAngular = true;
+					$scope.GetData = response.data.html;
+				}, function errorCallback(response) {
+				});
+			}
+		});
+		app.filter('safeHtml', function ($sce) {
+			return function (val) {
+				return $sce.trustAsHtml(val);
+			};
+		});
+		</script>
 </body>
 </html>
